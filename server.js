09024568e74af4317e94019d4e323e8cd2b9d6db
hemanth31196 <30641949+hemanth31196@users.noreply.github.com>
2017-08-10 12:09:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,16 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var wow = {
+    title: 'HI',
+    heading: 'What to do What not to do',
+    date:'september 2017',
+    context:`how to create a html file
+                 
+                    View your document with an internet browser. ...
+                    Understand markup tags. ...
+                    Write your first tag. ...
+                    Fill out theportion of your document. ...
+                    Creat section. ...
+                    Add text in various styles. ...
+                    Divide your text paragraphs`
+    
+};
+
+function create (data){
+    var title = data.title;
+    var date = data.date;
+    var context= data.context;
+var htmltemp =`<!DOCTYPE html>
+    <head>
+        <title>
+           $(title)
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <h5>What to do what not to do??</h5>
+        <div>
+            $(date)<br>
+            <p class='text'>$(context)
+            </p>
+        </div>
+            
+        
+    </body>
+</html>
+`;
+    
+return htmltemp;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(create(wow));
 });
 
 app.get('/read', function (req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'exp1.html'));
+   res.send(create(wow));
 });
 
 app.get('/write', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'exp2.html'));
+    res.send(create(wow));
 });
 
 app.get('/comic', function (req,res){
